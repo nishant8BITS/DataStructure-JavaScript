4c0382befe80a6e39b2aebe9ff5d881d93a9dcd9
Nishant Kumar <nihantanu@gmail.com>
2015-07-13 09:03:26 +0530

    Added BST basic functionality

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -20,3 +20,127 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
+ (function(){
+ 	'use strict';
+
+ 	// Define Node structure
+ 	var node = {
+ 		value : 124,
+ 		left  : null,
+ 		right : null
+ 	};
+
+ 	function BST(){
+ 		this.root = null;
+ 	}
+
+ 	BST.prototype = {
+ 		constructor: BST,
+ 		
+ 		add : function(value){
+ 			var node = {
+		 		value : value,
+		 		left  : null,
+		 		right : null
+		 	},
+
+		 	current;
+
+		 	if(this.root === null){
+		 		this.root = node;
+		 	}else{
+		 		current = this.root;
+		 		while(!!current){
+		 			if(value < current.value){
+		 				if(current.left === null){
+		 					current.left = node;
+		 					break;
+		 				}else{
+		 					current = current.left;
+		 				}
+		 			}else if(value > current.value){
+		 				if(current.right === null){
+		 					current.right = node;
+		 					break;
+		 				}else{
+		 					current = current.right;
+		 				}
+		 			}else {
+		 				throw "Duplicate Key can't be inserted";
+		 			}
+		 		}
+		 	}
+ 		},
+
+ 		remove : function(){
+
+ 		},
+
+ 		contains : function(value){
+ 			var current = this.root,
+ 				found = false;
+
+ 			while(!!current){
+ 				if(value > current.value){
+ 					current = current.right;
+ 				}else if(value < current.value){
+ 					current = current.left;
+ 				}else {
+ 					found = true;
+ 					break;
+ 				}
+ 			}
+ 			return found;
+ 		},
+
+ 		size : function(){
+
+ 		},
+
+ 		toString : function(){
+
+ 		},
+
+ 		toJson : function(){
+
+ 		},
+
+ 		toArray : function(){
+ 			var list = [];
+ 			this.traverse(function(node){
+ 				list.push(node.value);
+ 			});
+
+ 			return list;
+ 		},
+
+ 		traverse: function(process){
+ 			var self = this;
+ 			function inOrder(node){
+ 				if(!!node){
+ 					if(node.left !== null){
+ 						inOrder(node.left);
+ 					}
+
+ 					process.call(self, node);
+
+ 					if(node.right !== null){
+ 						inOrder(node.right);
+ 					}
+ 				}
+ 			}
+ 			inOrder(this.root);
+ 		}
+ 	};
+
+ 	var BinaryTree = new BST();
+ 	    BinaryTree.add(2);
+ 	    BinaryTree.add(3);
+ 	    BinaryTree.add(4);
+ 	    BinaryTree.add(9);
+ 	    BinaryTree.add(11);
+ 	    BinaryTree.add(1);
+ 	    console.log(BinaryTree.toArray());
+
+ })();
